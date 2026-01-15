65ed0787c61f684a15248132f7c792f1b2b6cd99
StanWeerman <stanweerman@gmail.com>
2026-01-15 01:10:29 -0800

    More Testbench Work

--- 8_bit_cpu/8_bit_cpu_tb.v
+++ 8_bit_cpu/8_bit_cpu_tb.v
@@ -12,36 +12,40 @@ module register_alu_tb;
         instruction = 16'b000_00000001_00101;
     endfunction
 
-    task add(input [2:0] dest, src1, src2);
+    task instr_clk();
         begin
-            instruction = {dest, src1, src2, 7'b0000001};
             #1;
             clk = 1;
             #1;
             clk = 0;
-            #1;
-
-            $display ("[$display] time=%0t $0=0x%00h", $time, cpu.register_file.reg_file[0]);
         end
     endtask
 
-    reg [2:0] dest, src1, src2;
+    task move(input [2:0] dest, input [7:0] val);
+        begin
+            instruction = {dest, val, 5'b00101};
+            instr_clk();
+            $display ("[$display] time=%0t $0=0x%00h", $time, cpu.register_file.reg_file[dest]);
+        end
+    endtask
+
+    task add(input [2:0] dest, src1, src2);
+        begin
+            instruction = {dest, src1, src2, 7'b0000001};
+            instr_clk();
+            $display ("[$display] time=%0t $0=0x%00h", $time, cpu.register_file.reg_file[dest]);
+        end
+    endtask
+
     integer i;
 
     initial begin
         init();
-        #1;
-        clk = 1;
-        #1;
-        clk = 0;
-        #1;
-        $display ("[$display] time=%0t $0=0x%00h", $time, cpu.register_file.reg_file[0]);
 
-        dest = 0;
-        src1 = 0;
-        src2 = 0;
+        move(0, 1);
+
         for (i=0; i<10; i++) begin
-            add(dest, src1, src2);
+            add(0, 0, 0);
         end
     end
 
