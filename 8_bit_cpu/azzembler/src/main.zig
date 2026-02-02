46b9675d9990d404acce15a5411bc605408e34ac
StanWeerman <stanweerman@gmail.com>
2026-02-02 01:49:29 -0800

    Working on Azzembler

--- 8_bit_cpu/azzembler/src/main.zig
+++ 8_bit_cpu/azzembler/src/main.zig
@@ -2,26 +2,17 @@ const std = @import("std");
 const azzembler = @import("azzembler");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try azzembler.bufferedPrint();
-}
+    const count = std.os.argv.len;
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    if (count == 2) {
+        const assembler = try azzembler.Assembler.init(allocator, std.os.argv[1]);
+        defer assembler.deinit();
+        assembler.assemble();
+    } else {
+        std.debug.print("Usage: azzembler program.s\n", .{});
+    }
 }
--- 8_bit_cpu/azzembler/src/root.zig
+++ 8_bit_cpu/azzembler/src/root.zig
@@ -1,5 +1,54 @@
-//! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+pub const Assembler = struct {
+    const Self = @This();
+    const cwd = std.fs.cwd();
+
+    allocator: Allocator,
+
+    asm_file: std.fs.File,
+    bin_file: std.fs.File,
+    v_file: std.fs.File,
+
+    pub fn init(allocator: Allocator, file_path: [*:0]u8) !Self {
+        cwd.deleteTree("build") catch |err| {
+            std.debug.print("Error: {any}\n", .{err});
+            // if (err == std.fs.Dir.DeleteTreeError)
+        };
+
+        try cwd.makeDir("build");
+
+        const asm_path: [:0]const u8 = std.mem.span(file_path);
+        const bin_path = try std.fs.path.join(allocator, &[_][]const u8{ "build/", std.fs.path.replaceExt(allocator, std.fs.path.basename(asm_path), ".bin") });
+        const v_path = try std.fs.path.join(allocator, &[_][]const u8{ "build/", std.fs.path.replaceExt(allocator, std.fs.path.basename(asm_path), ".v") });
+
+        const asm_file = try cwd.openFile(asm_path, .{ .mode = .read_only });
+        const bin_file = try cwd.openFile(bin_path, .{ .mode = .read_only });
+        const v_file = try cwd.openFile(v_path, .{ .mode = .read_only });
+
+        std.debug.print("Assembling {s}\n", .{asm_file});
+        allocator.free(bin_path);
+        allocator.free(v_path);
+
+        return Self{
+            .allocator = allocator,
+            .asm_file = asm_file,
+            .bin_file = bin_file,
+            .v_file = v_file,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.asm_file.close();
+        self.bin_file.close();
+        self.v_file.close();
+    }
+
+    pub fn assemble(self: *Self) void {
+        _ = self;
+    }
+};
 
 pub fn bufferedPrint() !void {
     // Stdout is for the actual output of your application, for example if you
@@ -12,12 +61,4 @@ pub fn bufferedPrint() !void {
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
     try stdout.flush(); // Don't forget to flush!
-}
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
 }
